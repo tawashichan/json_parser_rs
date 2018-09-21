db23044a24df6504371facce9d2d753b245865b6
irieryota <ryotairie123@gmail.com>
2018-09-22 00:17:07 +0900

    不要なコメント削除

--- src/ast.rs
+++ src/ast.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 
 #[derive(Debug,Clone)]
 pub enum Json {
-    //Assoc(Vec<(String,Json)>),
     Int(i64),
     Float(f64),
     String(String),
--- src/parser.rs
+++ src/parser.rs
@@ -2,31 +2,11 @@ use lexer::Token;
 use ast::Json;
 use std::collections::HashMap;
 
-//クソ遅いので、極力vecではなくslice,Stringではなく&strを使うように書き換える
 
 fn parse_list(tokens: &[Token]) -> Json {
-    //parse_list_sub(tokens,&mut vec![])
     let (_,json) = parse_list_sub(tokens,&mut HashMap::new());
     json
 }
-
-/*fn parse_list_sub<'a>(tokens: &'a[Token],acm: & mut Vec<(String,Json)>) -> (&'a[Token],Json) {
-    match tokens {
-        [Token::STRING(s),Token::COLON,rest..] => {
-            let (res,ac) = parse_list_sub(rest,&mut vec![]);
-            acm.push((s.clone(),ac));
-            parse_list_sub(res,acm)
-        }
-        [Token::RBRACE,rest..] => (rest,Json::Assoc(acm.clone())),
-        [Token::COMMA, rest..] => parse_list_sub(rest,acm),
-        [Token::INT(i),rest..] => (rest,Json::Int(*i)),
-        [Token::STRING(s),rest..] => (rest,Json::String(s.clone())),
-        [Token::FLOAT(f),rest..] => (rest,Json::Float(*f)),
-        [Token::LBRACE,rest..] => parse_list_sub(rest,acm),
-        [Token::LBRACKET,rest..] => parse_bracket(rest,&mut vec![]),
-        _ => (&[],Json::Assoc(acm.clone()))
-    }
-}*/
 
 fn parse_list_sub<'a>(tokens: &'a[Token],acm: &mut HashMap<String,Json>) -> (&'a[Token],Json) {
     match tokens {
